2b04e4ee67b0d06e0207927f8b0715adc98cd9b6
Yun <81588081+YunFeng-Zheng@users.noreply.github.com>
2023-03-22 22:03:44 +0100

    Add files via upload

---
+++ ReplyRetweetApp/src/main/java/com/QuackAttack/ReplyRetweetApp/ReplyRetweetAppApplication.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.ReplyRetweetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyRetweetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplyRetweetAppApplication.class, args);
+	}
+
+}
